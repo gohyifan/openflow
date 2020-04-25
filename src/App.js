02f2e0f804474c4a6ef5a810e8b5d25090a67780
JoaoNM <joao@ninamatos.com>
2020-04-25 15:40:35 +0800

    Added Contribution banner component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,34 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Dialog } from 'evergreen-ui';
+
+
+// import custom components
+import Banner from './components/contributeBanner';
+import Trending from './components/trending';
+
 function App() {
+  // initialize state of showLoginOverlay as false to hide it
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to OpenFlow
-        </p>
-      </header>
+      <div class="navbar">
+        NAV
+      </div>
+      <div className="sideview">
+        SIDE VIEW
+      </div>
+      <div className="body-wrapper">
+          <Banner
+          verified={true}
+          />
+       
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App
