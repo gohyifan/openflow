4b46fa6d488c06a406854045485091769d00f7f2
JoaoNM <joao@ninamatos.com>
2020-04-26 16:15:41 +0800

    Updates

---
+++ precache-manifest.03cd81db74c88b58221e1aedd5f69ee7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a83dca97393d2612d801bf7ab95e7f5",
+    "url": "/OpenFlow/index.html"
+  },
+  {
+    "revision": "62188ee2e55317d3ef6c",
+    "url": "/OpenFlow/static/css/main.f182b6a2.chunk.css"
+  },
+  {
+    "revision": "24ecb1d205b16bc3dc1a",
+    "url": "/OpenFlow/static/js/2.5c74bb09.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/OpenFlow/static/js/2.5c74bb09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62188ee2e55317d3ef6c",
+    "url": "/OpenFlow/static/js/main.8756431b.chunk.js"
+  },
+  {
+    "revision": "d622c60fc9a8ac714851",
+    "url": "/OpenFlow/static/js/runtime-main.c4544e7a.js"
+  },
+  {
+    "revision": "b3975cd19c3cf9a536a1017429be3b42",
+    "url": "/OpenFlow/static/media/illustration.b3975cd1.png"
+  },
+  {
+    "revision": "bb53b4373d2dbbdfc6aad5216884e1dc",
+    "url": "/OpenFlow/static/media/logo.bb53b437.svg"
+  }
+]);
